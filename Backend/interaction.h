f1c118bf2e7f3db3dc918d21b5f001dcba8ddf9c
SamarthH <47246334+SamarthH@users.noreply.github.com>
2020-04-10 14:25:49 +0530

    Update interaction.h

--- Backend/interaction.h
+++ Backend/interaction.h
@@ -1,6 +1,8 @@
 #include "system.h"
 #include <omp.h>
 
-void free_particles(){//The input variables still need to be considered
+double distance(simulation*, int a, int b); //Gives the distance between two particles labelled number a,b in the position array
 
-}
+void free_particles_periodic(simulation*);//Free particles with periodic boundary conditions
+
+void free_particles_box(simulation*);//Free particles contained within a box
