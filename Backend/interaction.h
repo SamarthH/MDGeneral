17b35d2eb1a3feb8cb3e25a3c31a9f16db124756
SamarthH <47246334+SamarthH@users.noreply.github.com>
2020-04-10 11:05:01 +0530

    updates

--- Backend/interaction.h
+++ Backend/interaction.h
@@ -1,1 +1,6 @@
 #include "system.h"
+#include <omp.h>
+
+void free_particles(){//The input variables still need to be considered
+
+}
--- Backend/system.h
+++ Backend/system.h
@@ -1,10 +1,17 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
 class system_state
 {
 public:
 	double*** position; //Needs to be allocated to have n_types X n_particles X n_dimensions size
 	double*** orientation; //Needs to be allocated to have n_types X n_particles X n_dimensions size
 	double*** velocity; //Needs to be allocated to have n_types X n_particles X n_dimensions sizes
-	
+	double* temperature; // This defines the temperatures of the n_types particle sets
 };
 
 class input_params
@@ -12,28 +19,40 @@ class input_params
 public:
 	int n_types; //This represents the number of types of particles
 	int n_dimensions; //This represents the number of dimensions of the simulation (by default must be 3)
-	int n_paticles[n_types]; //This represents the number of particles of each type
+	int* n_particles; //This represents the number of particles of each type (to be allocated to an n_types sized array)
 	double timestep;
 	double runtime;
 	int parallelize; // If 0, parallelize. Else, do not parallelize.
-	double mass[n_types]; //This represents the mass of each type of particle
-	void (*thermostat[n_types])(input_params*, system_state*); //This stores thermostats for different particle sets
-	void (*interaction[n_types][n_types])(double*, system_state*); //This defines the set of functions for interaction between different particle types. Also allows for non-symmetric interaction.
+	double* mass; //This represents the mass of each type of particle (to be allocated to an n_types sized array)
 	int periodic_boundary; //Use periodic boundary conditions if 1. If 0, use rigid walls.
-	double temper_init[n_types]; //This defines the initial temperatures of the n_types particle sets
 };
 
-class box : public input_params
+class simulation : public system_state, public input_params
 {
 public:
+	void (**thermostat)(input_params*, system_state*); //This stores thermostats for different particle sets
+	void (**interaction)(double*, system_state*); //This defines the set of functions for interaction between different particle types. Also allows for non-symmetric interaction.
 	double box_size_limits[n_dimensions]; // We assume that the initial limits are all (0,0,0,...,0) to whatever the limits define for a box.
-	box(double size[n_dimensions]){
+	simulation(int types, int dimensions, int n_par[types], double m[types], int parallel, int periodic, double time, double run){
+		n_types = types;
+		n_dimensions = dimensions;
+		//Allocating and defining n_particles
+		n_particles = new int[n_types];
+		for (int i = 0; i < n_ty; ++i)
+		{
+			n_particles[i] = n_par[i];
+		}
+		//Done
+		//Allocating and defining mass
+		mass = new int[n_types];
+		//Done
 		for (int i = 0; i < n_dimensions; ++i)
 		{
 			box_size_limits[i] = size[i];
 		}
 	};
-	~box();
+	~simulation();
 	
 };
 
+#endif
