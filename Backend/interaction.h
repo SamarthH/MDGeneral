5aad00dc3bdcb480ba4cf6ebc1d050fb54a7ac25
SamarthH <47246334+SamarthH@users.noreply.github.com>
2020-04-12 20:10:45 +0530

    Update interaction.h

--- Backend/interaction.h
+++ Backend/interaction.h
@@ -5,8 +5,8 @@
 
 double distance(simulation*, int type1, int n1, int type2, int n2); //Gives the distance between two particles labelled number n1,n2 of type1,type2 respectively in the position array
 
-void free_particles_periodic(simulation*);//Free particles with periodic boundary conditions
+void free_particles(simulation*);//Free particles
 
-void free_particles_box(simulation*);//Free particles contained within a box
+void lj_periodic(simulation*); //Lennard-Jones potential for periodic boundary conditions and cutoff = half the box size
 
 #endif
